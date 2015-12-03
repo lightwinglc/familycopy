d66ca6e931bc47a9928950e8d03960dcaa92c2c4
lightwinglc <lightwinglc@163.com>
2015-12-03 17:18:12 +0800

    基本功能完成

--- facp.py
+++ facp.py
@@ -8,6 +8,8 @@ import logging.config
 import sys
 import hashlib
 import sqlite3
+import datetime
+import shutil
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -24,8 +26,8 @@ class DBOperate(object):
                 self.cu.execute('''CREATE TABLE IF NOT EXISTS familyfiles
                                   (id integer primary key autoincrement,
                                    hashvalue text UNIQUE,
-                                   oldfilename text,
-                                   newfilename text)''')
+                                   srcfile text,
+                                   dstfile text)''')
                 self.cu.execute('''CREATE INDEX IF NOT EXISTS familyfiles_hashvalue_index
                                     ON familyfiles (hashvalue)''')
         except sqlite3.Error, msg:
@@ -49,29 +51,29 @@ class DBOperate(object):
         try:
             with self.conn:
                 checkedvalue = (hashvalue, )
-                self.cu.execute("select id, oldfilename from familyfiles where hashvalue = ?",
-                                checkedvalue)
+                self.cu.execute('''select id, srcfile, dstfile from familyfiles
+                                    where hashvalue = ?''', checkedvalue)
                 res = self.cu.fetchone()
                 if res is None:
                     return False
                 else:
-                    logger.info("hashvalue %s already existed, same as oldfile %s.",
-                                hashvalue, res[1])
+                    logger.info("hashvalue %s already existed, same as srcfile %s, dstfile %s.",
+                                hashvalue, res[1].encode("GBK"), res[2].encode("GBK"))
                     return True
         except sqlite3.Error, msg:
             logger.error("Check hash %s failed : %s.exit...", hashvalue, str(msg))
             exit()
 
     # 新增hash记录
-    def add_hash(self, hashvalue, oldfilename):
+    def add_hash(self, hashvalue, srcfile, dstfile):
         try:
             with self.conn:
-                record = (hashvalue, oldfilename)
-                self.cu.execute('''insert into familyfiles(hashvalue, oldfilename)
-                                    values(?, ?)''', record)
+                record = (hashvalue, srcfile, dstfile)
+                self.cu.execute('''insert into familyfiles(hashvalue, srcfile, dstfile)
+                                    values(?, ?, ?)''', record)
         except sqlite3.Error, msg:
-            logger.error("Add oldfilename %s, hash %s failed : %s.exit...",
-                         oldfilename, hashvalue, str(msg))
+            logger.error("DB add srcfile %s, hash %s failed : %s.exit...",
+                         srcfile.encode("GBK"), hashvalue, str(msg))
             exit()
 
 
@@ -81,9 +83,13 @@ class FamilyCopy(object):
         self.fadb = DBOperate()
         self.filehandled = 0
         self.fileignored = 0
+        self.picset = (['.bmp', '.dib', '.gif', '.png', '.rle', '.tif', '.tiff', '.bw',
+                        '.cdr', '.col', '.wmf', '.emf', '.ico', '.jpg', '.jpeg', '.pic'])
+        self.videoset = (['.mp4', '.rm', '.rmvb', '.avi', '.divx', '.mpg', '.mpeg', '.wmv',
+                          '.mov', '.asf', '.3gp', '.tp', '.ts'])
         # 保存成员变量
-        self.srcdir = srcdir.decode("GBK")
-        self.dstdir = dstdir.decode("GBK")
+        self.srcdir = srcdir
+        self.dstdir = dstdir
 
     # 计算文件hash加密，hash算法种类支持md5,sha1,sha256三种
     @staticmethod
@@ -105,18 +111,47 @@ class FamilyCopy(object):
                     break
                 hashobj.update(chunk)
             hashvalue = hashobj.hexdigest()
-            logger.debug("Calc file %s, type %s, hash value %s", inputfile, hashtype, hashvalue)
+            logger.debug("Calc file %s, type %s, hash value %s", inputfile.encode("GBK"), hashtype, hashvalue)
             return hashvalue
 
     # 处理源文件
     def handle_src_file(self, srcfile):
+        # 根据后缀区分照片和视频，未知后缀不处理
+        filetype = "picture"
+        filepart1, filepart2 = os.path.splitext(srcfile)
+        if filepart2 in self.picset:
+            pass
+        elif filepart2 in self.videoset:
+            filetype = "video"
+        else:
+            logger.warnning("file %s type not recognized, ignore", srcfile.encode("GBK"))
+            return None
+
+        # 计算文件hash，过滤重复文件
         hashvalue = self.calc_file_hash(srcfile, "sha256")
         if self.fadb.check_hash_existed(hashvalue):
-            logger.info("file %s already existed, ignore", srcfile)
+            logger.info("file %s already existed, ignore", srcfile.encode("GBK"))
             self.fileignored += 1
-        else:
-            self.fadb.add_hash(hashvalue, srcfile)
-            self.filehandled += 1
+            return None
+
+        # 取文件创建时间
+        srcfilectime = datetime.datetime.fromtimestamp(os.path.getctime(srcfile))
+        # 拼接目标文件目录
+        dstfilepath = os.path.join(self.dstdir, filetype, str(srcfilectime.year),
+                                   str(srcfilectime.month), str(srcfilectime.day))
+        if not os.path.isdir(dstfilepath):
+            os.makedirs(dstfilepath)
+            logger.info("Create directory %s succeed.", dstfilepath.encode("GBK"))
+        # 拼接目标文件
+        splitfilepath, splitfilename = os.path.split(srcfile)
+        dstfile = os.path.join(dstfilepath, splitfilename)
+        # 复制文件
+        shutil.copyfile(srcfile, dstfile)
+        logger.debug("File %s copy to %s.", srcfile.encode("GBK"), dstfile.encode("GBK"))
+
+        # 插入数据库记录
+        self.fadb.add_hash(hashvalue, srcfile, dstfile)
+        self.filehandled += 1
 
     # 遍历源文件目录
     def handle_src_dir(self):
@@ -133,38 +168,41 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("src", help="source file directory")
     parser.add_argument("dst", help="destination file directory")
-    parser.add_argument("-c", "--config", help="path of config file")
+    # parser.add_argument("-c", "--config", help="path of config file")
     args = parser.parse_args()
+    srcdir = args.src.decode("GBK")
+    dstdir = args.dst.decode("GBK")
 
     # 校验参数
     # 源文件目录是否存在
     logger.info("Check source directory:")
-    if os.path.isdir(args.src):
-        logger.info("%s is existed directory.", args.src)
+    if os.path.isdir(srcdir):
+        logger.info("%s is existed directory.", srcdir.encode("GBK"))
     else:
-        logger.error("%s is not directory, exit...", args.src)
+        logger.error("%s is not directory, exit...", srcdir.encode("GBK"))
         exit()
 
     # 目标文件目录是否存在，不存在则提醒用户新建
     logger.info("Check destination directory:")
-    if os.path.isdir(args.dst):
-        logger.info("%s is existed directory.", args.dst)
+    if os.path.isdir(dstdir):
+        logger.info("%s is existed directory.", dstdir.encode("GBK"))
     else:
-        userchoice = raw_input("%s is not directory. Need to create? (y/n)" % args.dst)
+        userchoice = raw_input("%s is not directory. Need to create? (y/n)" % dstdir.encode("GBK"))
         cpchoice = userchoice.lower()
         if "yes" == cpchoice or "y" == cpchoice:
             # 创建目录
             try:
-                os.makedirs(args.dst)
+                os.makedirs(dstdir)
             except OSError, msg:
-                logger.error("Create directory %s failed : %s.exit...", args.dst, str(msg))
+                logger.error("Create directory %s failed : %s.exit...", dstdir.encode("GBK"), str(msg))
                 exit()
             else:
-                logger.info("Create directory %s succeed.", args.dst)
+                logger.info("Create directory %s succeed.", dstdir.encode("GBK"))
         else:
             logger.info("Choose not to create directory, exit...")
             exit()
 
+    """
     # 配置文件
     if args.config:
         if os.path.isfile(args.config):
@@ -172,9 +210,10 @@ def main():
             pass
         else:
             logger.info("%s is not file, use default config file", args.config)
+    """
 
     # 使用FamilyCopy类
-    facp = FamilyCopy(args.src, args.dst)
+    facp = FamilyCopy(srcdir, dstdir)
     facp.handle_src_dir()
 
 # 初始化日志系统
