61b0851190cd84d30c437aa066ba3a4bf2e220e8
lightwinglc <lightwinglc@163.com>
2015-12-04 14:18:31 +0800

    功能调整
    1、日志级别调整；
    2、增加打印信息以提升人机交互的沟通感；

--- facp.py
+++ facp.py
@@ -35,7 +35,7 @@ class DBOperate(object):
             self.erase_resource()
             exit()
         else:
-            logger.info("Generate table familyfiles succeed.")
+            logger.debug("Generate table familyfiles succeed.")
         # finally:
             # self.conn.close()
 
@@ -57,8 +57,8 @@ class DBOperate(object):
                 if res is None:
                     return False
                 else:
-                    logger.info("hashvalue %s already existed, same as srcfile %s, dstfile %s.",
-                                hashvalue, res[1].encode("GBK"), res[2].encode("GBK"))
+                    logger.debug("hashvalue %s already existed, with srcfile %s, dstfile %s.",
+                                 hashvalue, res[1].encode("GBK"), res[2].encode("GBK"))
                     return True
         except sqlite3.Error, msg:
             logger.error("Check hash %s failed : %s.exit...", hashvalue, str(msg))
@@ -124,13 +124,13 @@ class FamilyCopy(object):
         elif filepart2 in self.videoset:
             filetype = "video"
         else:
-            logger.warnning("file %s type not recognized, ignore", srcfile.encode("GBK"))
+            logger.warnning("file %s is illegal, ignore", srcfile.encode("GBK"))
             return None
 
         # 计算文件hash，过滤重复文件
         hashvalue = self.calc_file_hash(srcfile, "sha256")
         if self.fadb.check_hash_existed(hashvalue):
-            logger.info("file %s already existed, ignore", srcfile.encode("GBK"))
+            logger.warning("file %s already existed, ignore", srcfile.encode("GBK"))
             self.fileignored += 1
             return None
 
@@ -151,7 +151,7 @@ class FamilyCopy(object):
             except OSError, msg:
                 logger.error("Create directory %s failed : %s.exit...", dstfilepath.encode("GBK"), str(msg))
             else:
-                logger.info("Create directory %s succeed.", dstfilepath.encode("GBK"))
+                logger.debug("Create directory %s succeed.", dstfilepath.encode("GBK"))
         # 拼接目标文件
         splitfilepath, splitfilename = os.path.split(srcfile)
         dstfile = os.path.join(dstfilepath, splitfilename)
@@ -169,11 +169,14 @@ class FamilyCopy(object):
             for filename in filenames:
                 fullfilename = os.path.join(parentdir, filename)
                 self.handle_src_file(fullfilename)
-        logger.info("%d files was handled, %d files was ignored.", self.filehandled, self.fileignored)
+                if self.filehandled % 100 == 0:
+                    print "%d files was handled, %d files was ignored." % (self.filehandled, self.fileignored)
+        print "All Done!"
+        logger.debug("%d files was handled, %d files was ignored.", self.filehandled, self.fileignored)
 
 
 def main():
-    logger.info("Program started.")
+    logger.debug("Program started.")
     # 命令行参数处理，接收两个定位参数：源文件目录和目标文件目录；一个可选参数：配置文件路径
     parser = argparse.ArgumentParser()
     parser.add_argument("src", help="source file directory")
@@ -185,17 +188,17 @@ def main():
 
     # 校验参数
     # 源文件目录是否存在
-    logger.info("Check source directory:")
+    logger.debug("Check source directory:")
     if os.path.isdir(srcdir):
-        logger.info("%s is existed directory.", srcdir.encode("GBK"))
+        logger.debug("%s is existed directory.", srcdir.encode("GBK"))
     else:
         logger.error("%s is not directory, exit...", srcdir.encode("GBK"))
         exit()
 
     # 目标文件目录是否存在，不存在则提醒用户新建
-    logger.info("Check destination directory:")
+    logger.debug("Check destination directory:")
     if os.path.isdir(dstdir):
-        logger.info("%s is existed directory.", dstdir.encode("GBK"))
+        logger.debug("%s is existed directory.", dstdir.encode("GBK"))
     else:
         userchoice = raw_input("%s is not directory. Need to create? (y/n)" % dstdir.encode("GBK"))
         cpchoice = userchoice.lower()
@@ -207,9 +210,9 @@ def main():
                 logger.error("Create directory %s failed : %s.exit...", dstdir.encode("GBK"), str(msg))
                 exit()
             else:
-                logger.info("Create directory %s succeed.", dstdir.encode("GBK"))
+                logger.debug("Create directory %s succeed.", dstdir.encode("GBK"))
         else:
-            logger.info("Choose not to create directory, exit...")
+            logger.warning("Choose not to create directory, exit...")
             exit()
 
     """
