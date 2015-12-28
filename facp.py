ddf03412795db8734ec6a2f28807a1111320d00f
lightwinglc <lightwinglc@163.com>
2015-12-28 17:35:12 +0800

    调整编码风格
    清除全局变量

--- facp.py
+++ facp.py
@@ -15,11 +15,56 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
+# 全局日志类
+class GlobalLog(object):
+    # 参数logdir为全局日志存放目录，支持绝对路径和相对路径
+    def __init__(self, logdir):
+        # 规范化路径
+        self.logdir = os.path.normpath(logdir)
+        if not os.path.isdir(self.logdir):
+            try:
+                os.makedirs(self.logdir)
+            except OSError, msg:
+                print "Create log directory %s failed : %s.exit..." % (self.logdir, str(msg))
+                exit()
+            else:
+                print "Create log directory %s succeed." % self.logdir
+        else:
+            print "Global log directory %s was already existed." % self.logdir
+
+        # 初始化日志系统
+        # CRITICAL > ERROR > WARNING > INFO > DEBUG > NOTSET
+        logging.config.fileConfig('logging.conf')
+        self.logger = logging.getLogger("familycopy")
+
+    # 获取全局日志
+    def get_logger(self):
+        return self.logger
+
+
 # 数据库操作类，暂时只支持sqlite
 class DBOperate(object):
-    def __init__(self):
+    # 初始化数据库建表
+    def __init__(self, dbfiledir, logger):
+        # 创建存放数据库文件的文件夹
+        self.dbfiledir = os.path.normpath(dbfiledir)
+        if not os.path.isdir(self.dbfiledir):
+            try:
+                os.makedirs(self.dbfiledir)
+            except OSError, msg:
+                print "Create db directory %s failed : %s.exit..." % (self.dbfiledir, str(msg))
+                exit()
+            else:
+                print "Create db directory %s succeed." % self.dbfiledir
+        else:
+            print "Global db directory %s was already existed." % self.dbfiledir
+
+        self.dbfile = os.path.join(self.dbfiledir, "facp.db")
+        self.logger = logger
+
+        # 连接数据库建表
         try:
-            self.conn = sqlite3.connect("db/facp.db")
+            self.conn = sqlite3.connect(self.dbfile)
             # self.conn.text_factory = str
             self.cu = self.conn.cursor()
             with self.conn:    # Using the connection as a context manager 自动commit()或者rollback()
@@ -46,7 +91,7 @@ class DBOperate(object):
     def __del__(self):
         self.erase_resource()
 
-    # 检查hash是否存在，返回True表示存在，False表示不存在
+    # 查表检查文件hash是否存在，返回True表示存在，False表示不存在
     def check_hash_existed(self, hashvalue):
         try:
             with self.conn:
@@ -57,14 +102,14 @@ class DBOperate(object):
                 if res is None:
                     return False
                 else:
-                    logger.debug("hashvalue %s already existed, with srcfile %s, dstfile %s.",
-                                 hashvalue, res[1].encode("GBK"), res[2].encode("GBK"))
+                    self.logger.debug("hashvalue %s already existed, with srcfile %s, dstfile %s.",
+                                      hashvalue, res[1].encode("GBK"), res[2].encode("GBK"))
                     return True
         except sqlite3.Error, msg:
-            logger.error("Check hash %s failed : %s.exit...", hashvalue, str(msg))
+            self.logger.error("Check hash %s failed : %s.exit...", hashvalue, str(msg))
             exit()
 
-    # 新增hash记录
+    # 新增文件hash记录
     def add_hash(self, hashvalue, srcfile, dstfile):
         try:
             with self.conn:
@@ -72,15 +117,15 @@ class DBOperate(object):
                 self.cu.execute('''insert into familyfiles(hashvalue, srcfile, dstfile)
                                     values(?, ?, ?)''', record)
         except sqlite3.Error, msg:
-            logger.error("DB add srcfile %s, hash %s failed : %s.exit...",
-                         srcfile.encode("GBK"), hashvalue, str(msg))
+            self.logger.error("DB add srcfile %s, hash %s failed : %s.exit...",
+                              srcfile.encode("GBK"), hashvalue, str(msg))
             exit()
 
 
 # 处理复制文件并改名分类的类
 class FamilyCopy(object):
-    def __init__(self, srcdir, dstdir):
-        self.fadb = DBOperate()
+    def __init__(self, srcdir, dstdir, logger):
+        self.fadb = DBOperate("db", logger)
         self.filehandled = 0
         self.fileignored = 0
         self.picset = (['.bmp', '.dib', '.gif', '.png', '.rle', '.tif', '.tiff', '.bw',
@@ -90,6 +135,8 @@ class FamilyCopy(object):
         # 保存成员变量
         self.srcdir = srcdir
         self.dstdir = dstdir
+
+        self.logger = logger
 
     # 计算文件hash加密，hash算法种类支持md5,sha1,sha256三种
     @staticmethod
@@ -111,7 +158,7 @@ class FamilyCopy(object):
                     break
                 hashobj.update(chunk)
             hashvalue = hashobj.hexdigest()
-            logger.debug("Calc file %s, type %s, hash value %s", inputfile.encode("GBK"), hashtype, hashvalue)
+            # logger.debug("Calc file %s, type %s, hash value %s", inputfile.encode("GBK"), hashtype, hashvalue)
             return hashvalue
 
     # 处理源文件
@@ -124,13 +171,13 @@ class FamilyCopy(object):
         elif filepart2.lower() in self.videoset:
             filetype = "video"
         else:
-            logger.warning("file %s is illegal, ignore", srcfile.encode("GBK"))
+            self.logger.warning("file %s is illegal, ignore", srcfile.encode("GBK"))
             return None
 
         # 计算文件hash，过滤重复文件
         hashvalue = self.calc_file_hash(srcfile, "sha256")
         if self.fadb.check_hash_existed(hashvalue):
-            logger.warning("file %s already existed, ignore", srcfile.encode("GBK"))
+            self.logger.warning("file %s already existed, ignore", srcfile.encode("GBK"))
             self.fileignored += 1
             return None
 
@@ -149,15 +196,15 @@ class FamilyCopy(object):
             try:
                 os.makedirs(dstfilepath)
             except OSError, msg:
-                logger.error("Create directory %s failed : %s.exit...", dstfilepath.encode("GBK"), str(msg))
+                self.logger.error("Create directory %s failed : %s.exit...", dstfilepath.encode("GBK"), str(msg))
             else:
-                logger.debug("Create directory %s succeed.", dstfilepath.encode("GBK"))
+                self.logger.debug("Create directory %s succeed.", dstfilepath.encode("GBK"))
         # 拼接目标文件
         splitfilepath, splitfilename = os.path.split(srcfile)
         dstfile = os.path.join(dstfilepath, splitfilename)
         # 复制文件
         shutil.copy2(srcfile, dstfile)
-        logger.debug("File %s copy to %s.", srcfile.encode("GBK"), dstfile.encode("GBK"))
+        self.logger.debug("File %s copy to %s.", srcfile.encode("GBK"), dstfile.encode("GBK"))
 
         # 插入数据库记录
         self.fadb.add_hash(hashvalue, srcfile, dstfile)
@@ -172,10 +219,12 @@ class FamilyCopy(object):
                 if self.filehandled % 100 == 0:
                     print "%d files was handled, %d files was ignored." % (self.filehandled, self.fileignored)
         print "All Done!"
-        logger.info("%d files was handled, %d files was ignored.", self.filehandled, self.fileignored)
+        self.logger.info("%d files was handled, %d files was ignored.", self.filehandled, self.fileignored)
 
 
 def main():
+    logger = GlobalLog("log").get_logger()
+
     logger.debug("Program started.")
     # 命令行参数处理，接收两个定位参数：源文件目录和目标文件目录；一个可选参数：配置文件路径
     parser = argparse.ArgumentParser()
@@ -226,34 +275,9 @@ def main():
     """
 
     # 使用FamilyCopy类
-    facp = FamilyCopy(srcdir, dstdir)
+    facp = FamilyCopy(srcdir, dstdir, logger)
     facp.handle_src_dir()
 
-# 创建数据库和日志系统的文件夹
-dbdir = "db"
-if not os.path.isdir(os.path.join(os.curdir, dbdir)):
-    try:
-        os.makedirs(os.path.join(os.curdir, dbdir))
-    except OSError, osmsg:
-        print "Create directory %s failed : %s.exit..." % (os.path.join(os.curdir, dbdir), str(osmsg))
-        exit()
-    else:
-        print "Create directory %s succeed." % os.path.join(os.curdir, dbdir)
-
-logdir = "log"
-if not os.path.isdir(os.path.join(os.curdir, logdir)):
-    try:
-        os.makedirs(os.path.join(os.curdir, logdir))
-    except OSError, osmsg:
-        print "Create directory %s failed : %s.exit..." % (os.path.join(os.curdir, logdir), str(osmsg))
-        exit()
-    else:
-        print "Create directory %s succeed." % os.path.join(os.curdir, logdir)
-
-# 初始化日志系统
-# CRITICAL > ERROR > WARNING > INFO > DEBUG > NOTSET
-logging.config.fileConfig('logging.conf')
-logger = logging.getLogger("familycopy")
 
 if __name__ == '__main__':
     main()
